a92f039f92bc6f100ce629811c05984afcdb9011
Louise Searle <Louise@LS01.local>
2015-01-13 00:42:37 +0100

    Finished. Provinces not aggregated but too tired.

--- main.R
+++ main.R
@@ -3,13 +3,6 @@
 
 ## Lesson 6 Exercise: Find out which city in the Netherlands is the greenest.
 
-# Find the greenest city
-# In January
-# In August
-# On average over the year
-# Make at least one map to visualize the results
-# What about provinces (see ?aggregate())
-
 ## Load packages and source functions.
 library(raster)
 library(rgdal)
@@ -22,7 +15,8 @@ dir.create('data/', showWarnings=F)
 
 # Get data.
 download('https://github.com/GeoScripting-WUR/VectorRaster/raw/gh-pages/data/MODIS.zip', 'data/MODIS.zip', mode='auto', quiet=T)
-boundNL <- getData('GADM', country='NLD', level=3)
+citiesNL <- getData('GADM', country='NLD', level=3, path='data/')
+provincesNL <- getData('GADM', country='NLD', level=2, path='data/')
 
 # Unzip data.
 unzip('data/MODIS.zip', exdir='data/MODIS/')
@@ -33,15 +27,76 @@ modisBrick <- brick(modisFiles)
 
 ## Spatial Analysis.
 
-# Reproject into RD.
-boundUTM <- spTransform(boundNL, CRS(proj4string(modisBrick)))
+# Extract mean NDVI per city for time periods.
+citiesNDVI <- suppressWarnings(extract(modisBrick, citiesNL, fun=mean, na.rm=T, match.ID=F, sp=T))
+citiesNDVI$Annual <- rowMeans(citiesNDVI@data[15:26], na.rm=T)
 
-# Create cities data frame.
-cities <- data.frame(Name = boundUTM$NAME_2)
+# citiesNDVI$Annual <- suppressWarnings(extract(mean(modisBrick), citiesNL, fun=mean, na.rm=T, match.ID=F, sp=T))
 
-# Extract mean NDVI per city and store in dataframe.
-cities$January <- extract(modisBrick[[1]], boundUTM, fun=mean, na.rm=T, match.ID=F)
-cities$August <- extract(modisBrick[[8]], boundUTM, fun=mean, na.rm=T, match.ID=F)
+# Extract mean NDVI per province for time periods.
+provincesNDVI <- suppressWarnings(extract(modisBrick, provincesNL, fun=mean, na.rm=T, match.ID=F, sp=T))
+provincesNDVI$Annual <- rowMeans(provincesNDVI@data[13:24], na.rm=T)
+
+# Greenest city calculation.
+cityJan <- subset(citiesNDVI, citiesNDVI$January == max(citiesNDVI$January), na.rm=T)
+cityAug <- subset(citiesNDVI, citiesNDVI$August == max(citiesNDVI$August), na.rm=T)
+cityAnnual <- subset(citiesNDVI, citiesNDVI$Annual == max(citiesNDVI$Annual), na.rm=T)
+
+# Greenest province calculation.
+provinceJan <- subset(provincesNDVI, provincesNDVI$January == max(provincesNDVI$January), na.rm=T)
+provinceAug <- subset(provincesNDVI, provincesNDVI$August == max(provincesNDVI$August), na.rm=T)
+provinceAnnual <- subset(provincesNDVI, provincesNDVI$Annual == max(provincesNDVI$Annual), na.rm=T)
 
 ## Visualise Results.
- 
+
+# Plot monthly city mean NDVI for August, January and Annually.
+spplot(citiesNDVI, zcol='January', col.regions=colorRampPalette(c('peachpuff4', 'lightgoldenrod', 'darkgreen'))(20), main='Mean NDVI January per City')
+spplot(citiesNDVI, zcol='August', col.regions=colorRampPalette(c('peachpuff4', 'lightgoldenrod', 'darkgreen'))(20), main='Mean NDVI August per City')
+spplot(citiesNDVI, zcol='Annual', col.regions=colorRampPalette(c('peachpuff4', 'lightgoldenrod', 'darkgreen'))(20), main='Mean NDVI Annual per City')
+
+# Plot monthly province mean NDVI for August, January and Annually.
+spplot(provincesNDVI, zcol='January', col.regions=colorRampPalette(c('peachpuff4', 'lightgoldenrod', 'darkgreen'))(20), main='Mean NDVI January per City')
+spplot(provincesNDVI, zcol='August', col.regions=colorRampPalette(c('peachpuff4', 'lightgoldenrod', 'darkgreen'))(20), main='Mean NDVI August per City')
+spplot(provincesNDVI, zcol='Annual', col.regions=colorRampPalette(c('peachpuff4', 'lightgoldenrod', 'darkgreen'))(20), main='Mean NDVI Annual per City')
+
+# Max NDVI city for August, January and Annually.
+opar <- par(mfrow=c(1,3))
+
+plot(citiesNDVI, col='honeydew2', axes=T, lwd=0.3)
+box()
+plot(cityJan, add=T, col='greenyellow')
+mtext(side=3, "Greenest City: January", line=1, adj=0)
+text(cityJan, labels=as.character(cityJan$NAME_2), font=2, pos=2, offset=0.8, col='darkgreen')
+
+plot(citiesNDVI, col='honeydew2', axes=T, lwd=0.3)
+box()
+plot(cityAug, add=T, col='violetred1')
+mtext(side=3, "Greenest City: August", line=1, adj=0)
+text(cityAug, labels=as.character(cityAug$NAME_2), font=2, pos=2, offset=0.8, col='violetred3')
+
+plot(citiesNDVI, col='honeydew2', axes=T, lwd=0.3)
+box()
+plot(cityAnnual, add=T, col='skyblue1')
+mtext(side=3, "Greenest City: Annual", line=1, adj=0)
+text(cityAnnual, labels=as.character(cityAnnual$NAME_2), font=2, pos=2, offset=0.8, col='steelblue4')
+
+# Max NDVI province for August, January and Annually.
+opar <- par(mfrow=c(1,3))
+
+plot(provincesJan, col='honeydew2', axes=T, lwd=0.3)
+box()
+plot(provinceJan, add=T, col='greenyellow')
+mtext(side=3, "Greenest Province: January", line=1, adj=0)
+text(provinceJan, labels=as.character(provinceJan$NAME_1), font=2, pos=2, offset=0.8, col='darkgreen')
+
+plot(provincesNDVI, col='honeydew2', axes=T, lwd=0.3)
+box()
+plot(provinceAug, add=T, col='violetred1')
+mtext(side=3, "Greenest Province: August", line=1, adj=0)
+text(provinceAug, labels=as.character(provinceAug$NAME_1), font=2, pos=2, offset=0.8, col='violetred3')
+
+plot(provincesNDVI, col='honeydew2', axes=T, lwd=0.3)
+box()
+plot(provinceAnnual, add=T, col='skyblue1')
+mtext(side=3, "Greenest Province: Annual", line=1, adj=0)
+text(provinceAnnual, labels=as.character(provinceAnnual$NAME_1), font=2, pos=2, offset=0.8, col='steelblue4')
